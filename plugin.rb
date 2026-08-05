2522beddb272a0c2ff60d7cf0f02ca47fe1cde36
JH-0417 <rjg6844@gmail.com>
2026-08-05 21:56:42 +0000

    Initial Discourse autograder plugin

---
+++ plugin.rb
@@ -0,0 +1,11 @@
+# frozen_string_literal: true
+
+# name: discourse-autograder
+# about: Python 자동채점 결과를 저장하고 개인·팀 랭킹을 표시합니다.
+# version: 0.1
+# authors: ahyeon
+
+enabled_site_setting :autograder_enabled
+
+after_initialize do
+end
