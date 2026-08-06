1b4ae8f4bcef59e7a85c577faf37ac1b967b38cb
JH-0417 <rjg6844@gmail.com>
2026-08-06 21:25:40 +0000

    Render public leaderboard pages outside Ember preload

--- app/controllers/autograder/leaderboards_controller.rb
+++ app/controllers/autograder/leaderboards_controller.rb
@@ -4,6 +4,14 @@ module ::Autograder
   class LeaderboardsController < ::ApplicationController
     requires_plugin PLUGIN_NAME
 
+    skip_before_action :preload_json,
+                       :check_xhr,
+                       :redirect_to_login_if_required,
+                       :redirect_to_profile_if_required,
+                       only: %i[index show]
+
+    before_action :prepend_plugin_view_path, only: :index
+
     def index
       @individual = Leaderboard.individual
       @teams = Leaderboard.teams(@individual)
@@ -19,6 +27,12 @@ module ::Autograder
         individual: individual,
         teams: Leaderboard.teams(individual),
       }
+    end
+
+    private
+
+    def prepend_plugin_view_path
+      prepend_view_path Rails.root.join("plugins", PLUGIN_NAME, "app", "views")
     end
   end
 end
